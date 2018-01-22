89a0060faf4e6db5a0dffbcd6bd0ad77af87ae3a
Calvin Hew <2078855h@student.gla.ac.uk>
2018-01-22 18:20:39 +0000

    population scripts added

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdeafult('DJANGO_SETTINGS_MODULE',
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 
 import django
@@ -29,7 +29,7 @@ def populate():
         {"title": "How to Tango with Django",
          "url:": "http://www.tangowithdjango.com/"} ]
 
-    other-pages= [
+    other_pages= [
         {"title": "Bottle",
          "url": "http://bottlepy.org/docs/dev/"},
         {"title": "Flask",
@@ -39,6 +39,39 @@ def populate():
            "Django": {"pages": django_pages},
            "Other Frameworks": {"pages": other_pages} }
 
+    # The code below goes through the cats dctionary, then adds each category,
+    # and then adds all the associated pages for that category.
+    # if you are using Python 2.x then use cats.iteritems() see
+    # http://docs.quantifiedcode.com/python-anti-patterns/readability/
+    # for more information about how to iterate over a dictionary properly.
+
+
+
+
     for cat, cat_data in cats.items():
-        
+        c= add_cat(cat)
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"])
+
+    # Print out the categories we have added.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
+    def add_page(cat, title, url, views=0):
+        p=Page.objects.get_or_create(category=cat, title=title)[0]
+        p.url=url
+        p.views=views
+        p.save()
+        return p
+    
+    def add_cat(name):
+        c=Category.objects.get_or_create(name=name)[0]
+        c.save()
+        return c
+    
+    # Start execution here!
+    if __name__ == '__main__':
+        print("Starting Rango population script...")
+        populate()
         
